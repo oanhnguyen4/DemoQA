dedf972ef610d50767c5bcf4f663b6329cf9fb79
oanhnguyen4 <oanhnt1997@gmail.com>
2025-05-04 21:36:34 +0700

    thưc hanh chay testcase textbox page

--- src/main/java/pages/ElementsPage.java
+++ src/main/java/pages/ElementsPage.java
@@ -1,5 +1,20 @@
 package pages;
 
-public class ElementsPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class ElementsPage extends Page{
+
+	public ElementsPage(WebDriver dr) {
+		super(dr);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TextboxPage clickOnTextBox() {
+		
+		driver.findElement(By.xpath("//span[text() ='Text Box']")).click();
+		return new TextboxPage(driver);
+	}
 
 }
--- src/main/java/pages/TextboxPage.java
+++ src/main/java/pages/TextboxPage.java
@@ -1,5 +1,31 @@
 package pages;
 
-public class TextboxPage {
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class TextboxPage extends Page {
+
+	public TextboxPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	public void inputData(String fullName, String email, String currentAddress, String permanentAddress) {
+		driver.findElement(By.id("userName")).sendKeys(fullName);
+		driver.findElement(By.id("userEmail")).sendKeys(email);
+		driver.findElement(By.id("currentAddress")).sendKeys(currentAddress);
+		driver.findElement(By.id("permanentAddress")).sendKeys(permanentAddress);
+		WebElement submitButton = driver.findElement(By.id("submit"));
+		// scroll nút submit
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", submitButton);
+        js.executeScript("arguments[0].click();", submitButton);
+       
+	}
+	
 
 }
